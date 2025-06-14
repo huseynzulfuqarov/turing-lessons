110ce9b74515cc9729fda2ab135b3c5478dbc241
Huseyn Zulfugarov <zulfuqarovhuseyn57@gmail.com>
2025-06-14 19:45:05 +0400

    Second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,24 @@
 package org.example;
 
+
 public class Main {
     public static void main(String[] args) {
+        byte a1 = 100;
+        short a2 = 20000;
+        int a3 = 1500000000;
+        long a4 = 90000000000L;
+        float a5 = 3.5f;
+        double a6 = 3.234534535;
+        boolean a7 = true;
+        char a8 = 'A';
 
-            int a = 10;
-            int b = 20;
-
-            System.out.println(a + b);
+        System.out.println("a1 (byte): " + a1);
+        System.out.println("a2 (short): " + a2);
+        System.out.println("a3 (int): " + a3);
+        System.out.println("a4 (long): " + a4);
+        System.out.println("a5 (float): " + a5);
+        System.out.println("a6 (double): " + a6);
+        System.out.println("a7 (boolean): " + a7);
+        System.out.println("a8 (char): " + a8);
     }
 }
-
-
-
-
