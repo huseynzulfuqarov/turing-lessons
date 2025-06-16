17e242ccb3be970dd9aa1b0b110438e4944770e7
Huseyn Zulfugarov <zulfuqarovhuseyn57@gmail.com>
2025-06-16 20:15:31 +0400

    Season Task - First part

---
+++ src/main/java/org/example/Seasons.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Seasons {
+    public static void main(String[] args) {
+
+        int ay = 2;
+
+            System.out.println((ay >= 3 && ay <= 5) ? "Yaz" : (ay >= 6 && ay <= 8) ? "Yay" : (ay >= 9 && ay <= 11) ? "Payız" :((ay >= 1 && ay <= 2) || ay == 12) ? "Qış" : "Yanlış ay");
+
+    }
+}
