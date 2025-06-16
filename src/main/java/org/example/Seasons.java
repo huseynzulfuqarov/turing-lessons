04d283d187625fb76403701e459e73b732db5e52
Huseyn Zulfugarov <zulfuqarovhuseyn57@gmail.com>
2025-06-16 20:31:44 +0400

    Season Task - Third part: Rewrite all results in best format

--- src/main/java/org/example/Seasons.java
+++ src/main/java/org/example/Seasons.java
@@ -3,12 +3,19 @@ package org.example;
 public class Seasons {
     public static void main(String[] args) {
 
-        int ay = 2;
+        System.out.println("Aylara görə fəsillər:");
+
+        for(int ay = 1; ay <= 13; ay++){
+
+            System.out.printf("%02d: ", ay);
 
             System.out.println((ay >= 3 && ay <= 5) ? "Yaz" :
                                (ay >= 6 && ay <= 8) ? "Yay" :
                                (ay >= 9 && ay <= 11) ? "Payız" :
-                               ((ay >= 1 && ay <= 2) || ay == 12) ? "Qış" : "Yanlış ay");
+                               (ay == 1 || ay == 2 || ay == 12) ? "Qış" : "Yanlış ay");
+
+        }
+
 
     }
 }
