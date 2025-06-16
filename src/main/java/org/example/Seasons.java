4c31c84955f982cf9b11b6457adbb45498ab94c9
Huseyn Zulfugarov <zulfuqarovhuseyn57@gmail.com>
2025-06-16 20:20:36 +0400

    Season Task - Second part: Improved Code Readability

--- src/main/java/org/example/Seasons.java
+++ src/main/java/org/example/Seasons.java
@@ -5,7 +5,10 @@ public class Seasons {
 
         int ay = 2;
 
-            System.out.println((ay >= 3 && ay <= 5) ? "Yaz" : (ay >= 6 && ay <= 8) ? "Yay" : (ay >= 9 && ay <= 11) ? "Payız" :((ay >= 1 && ay <= 2) || ay == 12) ? "Qış" : "Yanlış ay");
+            System.out.println((ay >= 3 && ay <= 5) ? "Yaz" :
+                               (ay >= 6 && ay <= 8) ? "Yay" :
+                               (ay >= 9 && ay <= 11) ? "Payız" :
+                               ((ay >= 1 && ay <= 2) || ay == 12) ? "Qış" : "Yanlış ay");
 
     }
 }
