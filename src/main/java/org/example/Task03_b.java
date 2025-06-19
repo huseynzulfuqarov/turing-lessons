d3a9c8859b21c5ce3b06e11521e18a7f1bb68c40
Huseyn Zulfugarov <zulfuqarovhuseyn57@gmail.com>
2025-06-20 02:47:58 +0400

    Task 03 - b: #1

---
+++ src/main/java/org/example/Task03_b.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Task03_b {
+    public static void main(String[] args) {
+
+        System.err.println("Product list: Select 1 to 5");
+        System.out.println(
+                        "*********************************\n" +
+                        "      1: Chocolate Bar - $2.00\n" +
+                        "      2: Soda - $1.50\n" +
+                        "      3: Chips - $1.20\n" +
+                        "      4: Sandwich - $3.00\n" +
+                        "      5: Water - $1.00\n" +
+                        "*********************************");
+
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1: System.out.println("Chocolate Bar - $2.00"); break;
+            case 2: System.out.println("Soda - $1.50"); break;
+            case 3: System.out.println("Chips - $1.20"); break;
+            case 4: System.out.println("Sandwich - $3.00"); break;
+            case 5: System.out.println("Water - $1.00"); break;
+            default: System.out.println("Invalid selection. Please enter a number between 1 to 5"); break;
+
+        }
+    }
+}
