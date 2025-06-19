b36af46f62e6e0d3d7e740e9990408db7c20ab89
Huseyn Zulfugarov <zulfuqarovhuseyn57@gmail.com>
2025-06-20 03:05:40 +0400

    Task 03 - c: #1

---
+++ src/main/java/org/example/Task03_c.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Task03_c {
+    public static void main(String[] args) {
+        System.out.println("Enter a number:");
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d\n", number, i, number*i);
+        }
+    }
+}
