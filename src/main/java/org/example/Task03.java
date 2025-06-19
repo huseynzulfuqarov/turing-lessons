dc3c78cbd09b7fc36e76562007665badaba1cfb9
Huseyn Zulfugarov <zulfuqarovhuseyn57@gmail.com>
2025-06-20 02:18:54 +0400

    Task 3 - a: #1

---
+++ src/main/java/org/example/Task03.java
@@ -0,0 +1,34 @@
+package org.example;
+import java.util.Scanner;
+
+public class Task03 {
+    public static void main(String[] args) {
+
+        int score1, score2, score3, average;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the scores: ");
+        score1 = sc.nextInt();
+        score2 = sc.nextInt();
+        score3 = sc.nextInt();
+
+        average = (score1 + score2 + score3) / 3;
+
+        if((score1 < 40) && (score2 < 40) && (score3 < 40))
+            System.out.println("Failed - One or more exam scores are too low.");
+
+        else if((average >= 85) && (score1 >= 85) && (score2 >= 85) && (score3 >= 85))
+            System.out.println("Congratulations! You have earned a Certificate of Excellence.");
+
+        else if((average >= 70 && average < 85) && (score1 >= 60) && (score2 >= 60) && (score3 >= 60))
+            System.out.println("Well done! You have earned a Certificate of Achievement.");
+
+        else if(average >= 50 && average < 70)
+            System.out.println("You passed.");
+
+        else
+            System.out.println("Failed.");
+
+    }
+}
