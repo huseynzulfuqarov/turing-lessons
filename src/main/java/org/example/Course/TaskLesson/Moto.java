143baee407f36c2b2981a4e451d5e8872f33cbbd
Huseyn Zulfugarov <zulfuqarovhuseyn57@gmail.com>
2025-07-09 21:00:43 +0400

    MotoTask

---
+++ src/main/java/org/example/Course/TaskLesson/Moto.java
@@ -0,0 +1,24 @@
+package org.example.Course.TaskLesson;
+
+public class Moto {
+    String model;
+    String color;
+    double price;
+    double weight;
+    double power;
+    int year;
+
+    public Moto(String model, String color, double price, double weight, double power, int year) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+        this.weight = weight;
+        this.power = power;
+        this.year = year;
+    }
+
+    public String toString() {
+    return "Moto{model=%s, color=%s, price=%s, weight=%s, power=%s, year=%d}".formatted(this.model, this.color, this.price, this.weight, this.power, this.year);
+    }
+
+}
