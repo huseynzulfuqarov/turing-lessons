1c5e9b3604492308716379833d6e5a62d7c72022
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2025-08-07 21:21:51 +0400

    Wrapper learning

---
+++ src/main/java/org/example/Personal/Mixed/Wrapper.java
@@ -0,0 +1,15 @@
+package org.example.Personal.Mixed;
+
+public class Wrapper {
+
+    public static void main(String[] args) {
+
+        Integer number = Integer.valueOf(100);
+        Integer number2 = 200;
+
+        Integer number3 = new Integer(3000);
+
+        Integer wrapperNumber = 50;
+        double primitiveNumber = wrapperNumber;
+    }
+}
