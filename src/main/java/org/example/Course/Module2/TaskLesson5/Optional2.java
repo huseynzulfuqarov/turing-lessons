766c14f9b4fe3f96dc72476a4fae3474fc38f701
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2025-08-22 20:03:05 +0400

    Initial commit

--- src/main/java/org/example/Course/Module2/TaskLesson5/Optional2.java
+++ src/main/java/org/example/Course/Module2/TaskLesson5/Optional2.java
@@ -1,29 +1,29 @@
 package org.example.Course.Module2.TaskLesson5;
 
-public class Optional2 <T>{
-    T value;
+public class Optional2<T> {
+    private T value;
 
     public Optional2(T value) {
         this.value = value;
     }
 
     public boolean isEmpty() {
-        if(value == null){
+        if (value == null) {
             return true;
         }
         return false;
     }
 
     public boolean isPresent() {
-        if(value != null){
+        if (value != null) {
             return true;
         }
         return false;
     }
 
     public T getValue() {
-       boolean isEmpty = isEmpty();
-        if(!isEmpty){
+        boolean isEmpty = isEmpty();
+        if (!isEmpty) {
             return value;
         }
         return null;
