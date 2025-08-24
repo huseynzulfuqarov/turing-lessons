7ca65a1ff093c8e22f6a0c48a5e5b6117513adcb
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2025-08-25 01:51:54 +0400

    Created UserNotFoundException

---
+++ src/main/java/org/example/Course/Module2/TaskLesson6/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package org.example.Course.Module2.TaskLesson6.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
