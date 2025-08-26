1f487fb607f5fcca464eba4128add0112371ebd5
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2025-08-26 23:51:02 +0400

    Box app learning task

--- src/main/java/org/example/Course/Module2/TaskLesson4/BoxApp.java
+++ src/main/java/org/example/Course/Module2/TaskLesson4/BoxApp.java
@@ -2,6 +2,8 @@ package org.example.Course.Module2.TaskLesson4;
 
 import org.example.Course.Module2.TaskLesson4.model.BoxObject;
 
+import java.util.ArrayList;
+
 public class BoxApp {
 
     public static void main(String[] args) {
