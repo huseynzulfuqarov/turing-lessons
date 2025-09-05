46e4e38cce4c69748926635d55860ce3cacf9002
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2025-09-06 02:22:52 +0400

    complete custom LinkedList with full CRUD, clear, and toString; tested in main

--- src/main/java/org/example/Course/Module2/Task11/Main.java
+++ src/main/java/org/example/Course/Module2/Task11/Main.java
@@ -1,8 +1,60 @@
 package org.example.Course.Module2.Task11;
 
+import org.example.Course.Module2.Task11.model.CustomLinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        CustomLinkedList<String> list = new CustomLinkedList<>();
+
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("e");
+        list.add("f");
+        list.add("g");
+        list.add("h");
+        list.add("i");
+        list.add("j");
+        list.add("k");
+        list.add("l");
+        list.add("m");
+        list.add("n");
+        list.add("o");
+        list.add("p");
+        list.add("q");
+        list.add("r");
+        list.add("s");
+        list.add("t");
+        list.add("u");
+        list.add("v");
+        list.add("w");
+        list.add("x");
+        list.add("y");
+        list.add("z");
+
+        System.out.println(list);
+
+        System.out.println(list.get(5));
+
+        System.out.println(list.isEmpty());
+
+        System.out.println(list.size());
+
+        System.out.println(list.remove("d"));
+
+        list.set(3,"gg");
+
+        System.out.println(list);
+
+        list.clear();
+
+        System.out.println(list.isEmpty());
+
+        System.out.println(list.size());
+
+        System.out.println(list);
     }
 }
--- src/main/java/org/example/Course/Module2/Task11/model/CustomLinkedList.java
+++ src/main/java/org/example/Course/Module2/Task11/model/CustomLinkedList.java
@@ -16,20 +16,19 @@ public final class CustomLinkedList<T> {
     }
 
     public boolean isEmpty() {
-        return  size == 0;
+        return size == 0;
     }
 
     public void add(T t) {
         Node<T> newNode = new Node<>(t);
 
-        if(firstNode == null) {
-            firstNode =  newNode;
-        }
-        else {
+        if (firstNode == null) {
+            firstNode = newNode;
+        } else {
             lastNode.next = newNode; //cox mentiqli hisse, firstle eyni obyekt
             newNode.prev = lastNode;
         }
-        lastNode =  newNode;
+        lastNode = newNode;
         size++;
     }
 
@@ -44,27 +43,100 @@ public final class CustomLinkedList<T> {
             return false;
         }
 
-        if(current == firstNode) {
+        if (current == firstNode) {
             firstNode = firstNode.next;
-            if(firstNode != null) {
+            if (firstNode != null) {
                 firstNode.prev = null;
-            }
-            else {
+            } else {
                 lastNode = null;
             }
-        }
-
-        else if(current == lastNode) {
+        } else if (current == lastNode) {
             lastNode = lastNode.prev;
             lastNode.next = null;
-        }
-
-        else {
+        } else {
             current.prev.next = current.next;
             current.next.prev = current.prev;
         }
         size--;
         return true;
+    }
+
+    public T get(int index) {
+        Node<T> current;
+
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        if (index < size / 2) {
+            current = firstNode;
+
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            current = lastNode;
+
+            for (int i = size - 1; i > index; i--) {
+                current = current.prev;
+            }
+        }
+        return current.item;
+    }
+
+    public boolean contains(T data) {
+        Node<T> current = firstNode;
+        while (current != null) {
+            if (current.item.equals(data)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public void clear() {
+        firstNode = null;
+        lastNode = null;
+        size = 0;
+    }
+
+    public void set(int index, T data) {
+        Node<T> current;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        if (index <= size / 2) {
+            current = firstNode;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            current = lastNode;
+            for (int i = size - 1; i > index; i--) {
+                current = current.prev;
+            }
+        }
+        current.item = data;
+    }
+
+    public String toString() {
+        if (firstNode == null) {
+            return "[]";
+        }
+        StringBuilder sb = new StringBuilder("[");
+
+        Node<T> current = firstNode;
+        for (int i = 0; i < size; i++) {
+            sb.append(current.item);
+            current = current.next;
+            if (i != size - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
     }
 
     private static class Node<E> {
