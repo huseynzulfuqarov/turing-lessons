1a8568836a54ea258395a7b0eca0c5061c28d498
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2025-09-05 19:23:13 +0400

    Save alphabet in 3D array

---
+++ src/main/java/org/example/Course/Module2/TaskLesson8/ArrayApp.java
@@ -0,0 +1,27 @@
+package org.example.Course.Module2.TaskLesson8;
+
+import java.util.Arrays;
+
+public class ArrayApp {
+
+    public static void main(String[] args) {
+
+        String[][][] lettersArr3D = new String[3][3][3];
+
+        char letterC = 65;
+        char letterL = 97;
+        System.out.println(letterC);
+        for(int i=0; i<lettersArr3D.length; i++){
+            for(int j=0; j<lettersArr3D[i].length; j++){
+                for(int k=0; k<lettersArr3D[i][j].length; k++){
+
+                    String letter = String.valueOf(letterC) + String.valueOf(letterL);
+                    lettersArr3D[i][j][k] = letter;
+                    letterC++;
+                    letterL++;
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(lettersArr3D));
+    }
+}
